90a055b2898d631d833d40f1ea602624df37990e
Carl Wicker <carlwicker@gmail.com>
2024-06-03 23:29:02 +0100

    avitars added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import LoginForm from "@/components/LoginForm";
 import { SignIn } from "@/components/SignInButton";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await auth();
@@ -8,7 +9,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col justify-center w-full h-screen p-20 gap-10">
-      <h1 className="text-6xl uppercase font-bold">Utopia</h1>
+      <h1 className="text-6xl uppercase font-bold">Utopia//Engine</h1>
       <p className="text-2xl">
         Utopia represents a pioneering venture in AI-facilitated content
         generation, meticulously crafted to sculpt an infinitely vast and
@@ -17,8 +18,24 @@ export default async function Home() {
       <a href="mailto:carlwicker@gmail.com" className="text-2xl underline">
         carlwicker@gmail.com
       </a>
-      <p>Welcome {session?.user?.email}! You are logged in.</p>
-      <SignIn />
+
+      {!session ? (
+        <SignIn />
+      ) : (
+        <div className="flex flex-col gap-5">
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <p>Welcome {session?.user?.email}! You are logged in.</p>
+          <button className="bg-blue-400 p-5 rounded text-white max-w-min">
+            Enter
+          </button>
+        </div>
+      )}
     </main>
   );
 }
